bcdb42860dc6576b07700e917704048be366f15a
Marco <MarcoDavid67@hotmail.com>
2024-11-25 15:01:51 -0700

    Importar imag

--- GestorSalas/Vistas/AgregarPelicualas.Designer.cs
+++ GestorSalas/Vistas/AgregarPelicualas.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.nombrepTxb = new System.Windows.Forms.TextBox();
             this.NombreP = new System.Windows.Forms.Label();
             this.GeneroP = new System.Windows.Forms.Label();
@@ -35,40 +36,47 @@
             this.generoCbx = new System.Windows.Forms.ComboBox();
             this.duracionPud = new System.Windows.Forms.NumericUpDown();
             this.agregarPbtn = new System.Windows.Forms.Button();
+            this.imagenPelBtn = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.RutaImagenTxt = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.duracionPud)).BeginInit();
             this.SuspendLayout();
             // 
             // nombrepTxb
             // 
-            this.nombrepTxb.Location = new System.Drawing.Point(80, 40);
+            this.nombrepTxb.Location = new System.Drawing.Point(107, 49);
+            this.nombrepTxb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.nombrepTxb.Name = "nombrepTxb";
-            this.nombrepTxb.Size = new System.Drawing.Size(121, 20);
+            this.nombrepTxb.Size = new System.Drawing.Size(160, 22);
             this.nombrepTxb.TabIndex = 0;
             // 
             // NombreP
             // 
             this.NombreP.AutoSize = true;
-            this.NombreP.Location = new System.Drawing.Point(242, 43);
+            this.NombreP.Location = new System.Drawing.Point(323, 53);
+            this.NombreP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NombreP.Name = "NombreP";
-            this.NombreP.Size = new System.Drawing.Size(44, 13);
+            this.NombreP.Size = new System.Drawing.Size(56, 16);
             this.NombreP.TabIndex = 1;
             this.NombreP.Text = "Nombre";
             // 
             // GeneroP
             // 
             this.GeneroP.AutoSize = true;
-            this.GeneroP.Location = new System.Drawing.Point(242, 81);
+            this.GeneroP.Location = new System.Drawing.Point(323, 100);
+            this.GeneroP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.GeneroP.Name = "GeneroP";
-            this.GeneroP.Size = new System.Drawing.Size(42, 13);
+            this.GeneroP.Size = new System.Drawing.Size(52, 16);
             this.GeneroP.TabIndex = 3;
             this.GeneroP.Text = "Genero";
             // 
             // DuracionP
             // 
             this.DuracionP.AutoSize = true;
-            this.DuracionP.Location = new System.Drawing.Point(242, 120);
+            this.DuracionP.Location = new System.Drawing.Point(323, 148);
+            this.DuracionP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DuracionP.Name = "DuracionP";
-            this.DuracionP.Size = new System.Drawing.Size(89, 13);
+            this.DuracionP.Size = new System.Drawing.Size(110, 16);
             this.DuracionP.TabIndex = 5;
             this.DuracionP.Text = "Duracion minutos";
             // 
@@ -76,33 +84,62 @@
             // 
             this.generoCbx.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.generoCbx.FormattingEnabled = true;
-            this.generoCbx.Location = new System.Drawing.Point(80, 81);
+            this.generoCbx.Location = new System.Drawing.Point(107, 100);
+            this.generoCbx.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.generoCbx.Name = "generoCbx";
-            this.generoCbx.Size = new System.Drawing.Size(121, 21);
+            this.generoCbx.Size = new System.Drawing.Size(160, 24);
             this.generoCbx.TabIndex = 6;
             // 
             // duracionPud
             // 
-            this.duracionPud.Location = new System.Drawing.Point(81, 118);
+            this.duracionPud.Location = new System.Drawing.Point(108, 145);
+            this.duracionPud.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.duracionPud.Name = "duracionPud";
-            this.duracionPud.Size = new System.Drawing.Size(120, 20);
+            this.duracionPud.Size = new System.Drawing.Size(160, 22);
             this.duracionPud.TabIndex = 7;
             // 
             // agregarPbtn
             // 
-            this.agregarPbtn.Location = new System.Drawing.Point(126, 176);
+            this.agregarPbtn.Location = new System.Drawing.Point(149, 261);
+            this.agregarPbtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.agregarPbtn.Name = "agregarPbtn";
-            this.agregarPbtn.Size = new System.Drawing.Size(75, 23);
+            this.agregarPbtn.Size = new System.Drawing.Size(100, 28);
             this.agregarPbtn.TabIndex = 8;
             this.agregarPbtn.Text = "Agregar";
             this.agregarPbtn.UseVisualStyleBackColor = true;
             this.agregarPbtn.Click += new System.EventHandler(this.agregarPbtn_Click);
             // 
+            // imagenPelBtn
+            // 
+            this.imagenPelBtn.Location = new System.Drawing.Point(300, 207);
+            this.imagenPelBtn.Name = "imagenPelBtn";
+            this.imagenPelBtn.Size = new System.Drawing.Size(136, 24);
+            this.imagenPelBtn.TabIndex = 9;
+            this.imagenPelBtn.Text = "Importar imagen";
+            this.imagenPelBtn.UseVisualStyleBackColor = true;
+            this.imagenPelBtn.Click += new System.EventHandler(this.imagenPelBtn_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // RutaImagenTxt
+            // 
+            this.RutaImagenTxt.Location = new System.Drawing.Point(107, 207);
+            this.RutaImagenTxt.Name = "RutaImagenTxt";
+            this.RutaImagenTxt.ReadOnly = true;
+            this.RutaImagenTxt.Size = new System.Drawing.Size(161, 22);
+            this.RutaImagenTxt.TabIndex = 10;
+            // 
             // AgregarPelicualas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(336, 256);
+            this.ClientSize = new System.Drawing.Size(448, 315);
+            this.Controls.Add(this.RutaImagenTxt);
+            this.Controls.Add(this.imagenPelBtn);
             this.Controls.Add(this.agregarPbtn);
             this.Controls.Add(this.duracionPud);
             this.Controls.Add(this.generoCbx);
@@ -110,7 +147,7 @@
             this.Controls.Add(this.GeneroP);
             this.Controls.Add(this.NombreP);
             this.Controls.Add(this.nombrepTxb);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "AgregarPelicualas";
             this.Text = "AggregarPelicualas";
             this.Load += new System.EventHandler(this.AgregarPelicualas_Load);
@@ -129,5 +166,8 @@
         private System.Windows.Forms.ComboBox generoCbx;
         private System.Windows.Forms.NumericUpDown duracionPud;
         private System.Windows.Forms.Button agregarPbtn;
+        private System.Windows.Forms.Button imagenPelBtn;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TextBox RutaImagenTxt;
     }
 }
--- GestorSalas/Vistas/AgregarPelicualas.cs
+++ GestorSalas/Vistas/AgregarPelicualas.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace GestorSalas.Vistas
 {
     public partial class AgregarPelicualas : Form
     {
+        String ImagenRuta = "";
         public AgregarPelicualas()
         {
             InitializeComponent();
@@ -39,12 +41,14 @@ namespace GestorSalas.Vistas
         private void agregarPbtn_Click(object sender, EventArgs e)
         {
             baseDatosServicios dBserv = new baseDatosServicios();
-            if (nombrepTxb.Text == "" || generoCbx.SelectedIndex == -1)
+            if (nombrepTxb.Text == "" || generoCbx.SelectedIndex == -1 || ImagenRuta == "")
             {
                 MessageBox.Show("Porfavor rellene todos los parametros");
             }
             else
             {
+
+                GuardarImagen(ImagenRuta, "C:\\Users\\robol\\Documents\\GestorSalas(TallerDB)\\GestorSalas\\Assets\\", nombrepTxb.Text);
                 if (!dBserv.agregarPelicula(nombrepTxb.Text, generoCbx.SelectedItem.ToString(), (int)duracionPud.Value))
                 {
 
@@ -59,11 +63,57 @@ namespace GestorSalas.Vistas
                     duracionPud.Value =40;
                     generoCbx.SelectedIndex = -1;
                     nombrepTxb.Text = "";
-
+                    RutaImagenTxt.Text = "";
 
                 }
 
 
+            }
+        }
+
+        private void imagenPelBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "Seleccionar imagen",
+                Filter = "Archivos de imagen|*.jpg;*.jpeg;*.png;*.bmp;*.gif",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures)
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Actualizar el TextBox con la ruta seleccionada
+                RutaImagenTxt.Text= ImagenRuta = openFileDialog.FileName;
+
+            }
+           
+        }
+
+        private static void GuardarImagen(string rutaOriginal, string carpetaDestino, string nuevoNombre)
+        {
+            try
+            {
+                // Verificar si la ruta original existe
+                if (!File.Exists(rutaOriginal))
+                {
+                    MessageBox.Show("La imagen original no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+
+                // Obtener la extensión de la imagen original
+                string extension = Path.GetExtension(rutaOriginal);
+
+                // Combinar la carpeta de destino y el nuevo nombre
+                string rutaDestino = Path.Combine(carpetaDestino, nuevoNombre + extension);
+
+                // Copiar la imagen al destino con el nuevo nombre
+                File.Copy(rutaOriginal, rutaDestino, true);
+
+                MessageBox.Show($"La imagen se ha guardado como:\n{rutaDestino}", "Operación exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error al copiar la imagen:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
