1e9fada871af43deff0829c6839d4d1058baf83d
Martin Aburto <martinzahid@hotmail.com>
2024-11-25 01:53:37 -0700

    funcion nueva para que salgan imagenes y ademas se puedan selecccionar

--- GestorSalas/Vistas/Peliculas.Designer.cs
+++ GestorSalas/Vistas/Peliculas.Designer.cs
@@ -29,10 +29,7 @@
         private void InitializeComponent()
         {
             this.btnCambiarUsuario = new System.Windows.Forms.Button();
-            this.btnBuscarFuncion = new System.Windows.Forms.Button();
             this.gestionarBaseBtn = new System.Windows.Forms.Button();
-            this.peliculasDgv = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.peliculasDgv)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCambiarUsuario
@@ -42,28 +39,13 @@
             this.btnCambiarUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCambiarUsuario.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.btnCambiarUsuario.ForeColor = System.Drawing.Color.White;
-            this.btnCambiarUsuario.Location = new System.Drawing.Point(550, 320);
+            this.btnCambiarUsuario.Location = new System.Drawing.Point(559, 297);
             this.btnCambiarUsuario.Name = "btnCambiarUsuario";
             this.btnCambiarUsuario.Size = new System.Drawing.Size(200, 50);
             this.btnCambiarUsuario.TabIndex = 1;
             this.btnCambiarUsuario.Text = "Cambiar Usuario";
             this.btnCambiarUsuario.UseVisualStyleBackColor = false;
             this.btnCambiarUsuario.Click += new System.EventHandler(this.btnCambiarUsuario_Click);
-            // 
-            // btnBuscarFuncion
-            // 
-            this.btnBuscarFuncion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(167)))), ((int)(((byte)(69)))));
-            this.btnBuscarFuncion.FlatAppearance.BorderSize = 0;
-            this.btnBuscarFuncion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBuscarFuncion.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.btnBuscarFuncion.ForeColor = System.Drawing.Color.White;
-            this.btnBuscarFuncion.Location = new System.Drawing.Point(550, 50);
-            this.btnBuscarFuncion.Name = "btnBuscarFuncion";
-            this.btnBuscarFuncion.Size = new System.Drawing.Size(200, 50);
-            this.btnBuscarFuncion.TabIndex = 2;
-            this.btnBuscarFuncion.Text = "Buscar Función";
-            this.btnBuscarFuncion.UseVisualStyleBackColor = false;
-            this.btnBuscarFuncion.Click += new System.EventHandler(this.btnBuscarFuncion_Click);
             // 
             // gestionarBaseBtn
             // 
@@ -72,7 +54,7 @@
             this.gestionarBaseBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.gestionarBaseBtn.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.gestionarBaseBtn.ForeColor = System.Drawing.Color.White;
-            this.gestionarBaseBtn.Location = new System.Drawing.Point(550, 180);
+            this.gestionarBaseBtn.Location = new System.Drawing.Point(559, 74);
             this.gestionarBaseBtn.Name = "gestionarBaseBtn";
             this.gestionarBaseBtn.Size = new System.Drawing.Size(200, 50);
             this.gestionarBaseBtn.TabIndex = 3;
@@ -80,37 +62,17 @@
             this.gestionarBaseBtn.UseVisualStyleBackColor = false;
             this.gestionarBaseBtn.Click += new System.EventHandler(this.gestionarBaseBtn_Click);
             // 
-            // peliculasDgv
-            // 
-            this.peliculasDgv.AllowUserToAddRows = false;
-            this.peliculasDgv.AllowUserToDeleteRows = false;
-            this.peliculasDgv.AllowUserToResizeColumns = false;
-            this.peliculasDgv.AllowUserToResizeRows = false;
-            this.peliculasDgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.peliculasDgv.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.peliculasDgv.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
-            this.peliculasDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.peliculasDgv.Location = new System.Drawing.Point(20, 20);
-            this.peliculasDgv.Name = "peliculasDgv";
-            this.peliculasDgv.ReadOnly = true;
-            this.peliculasDgv.RowHeadersWidth = 51;
-            this.peliculasDgv.Size = new System.Drawing.Size(500, 400);
-            this.peliculasDgv.TabIndex = 0;
-            // 
             // Peliculas
             // 
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
             this.ClientSize = new System.Drawing.Size(800, 500);
-            this.Controls.Add(this.peliculasDgv);
             this.Controls.Add(this.btnCambiarUsuario);
-            this.Controls.Add(this.btnBuscarFuncion);
             this.Controls.Add(this.gestionarBaseBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Peliculas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestión de Películas";
             this.Activated += new System.EventHandler(this.Peliculas_Activated_1);
-            ((System.ComponentModel.ISupportInitialize)(this.peliculasDgv)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -119,8 +81,6 @@
         #endregion
 
         private System.Windows.Forms.Button btnCambiarUsuario;
-        private System.Windows.Forms.Button btnBuscarFuncion;
         private System.Windows.Forms.Button gestionarBaseBtn;
-        private System.Windows.Forms.DataGridView peliculasDgv;
     }
 }
--- GestorSalas/Vistas/Peliculas.cs
+++ GestorSalas/Vistas/Peliculas.cs
@@ -3,17 +3,27 @@ using GestorSalas.Servicios;
 using GestorSalas.Vistas;
 using System;
 using System.Data;
+using System.IO;
 using System.Windows.Forms;
 
 namespace GestorSalas
 {
     public partial class Peliculas : Form
     {
+        private Panel panelPeliculas;  
+        private baseDatosServicios baseDatosServicios;
         public Empleado empleado;
         public Peliculas(Empleado empleado)
         {
+
             this.empleado = empleado;
             InitializeComponent();
+            InitializeComponent();
+            baseDatosServicios = new baseDatosServicios();  // Inicializar la clase baseDatosServicios
+            panelPeliculas = new Panel(); 
+            panelPeliculas.AutoScroll = true; 
+            panelPeliculas.Dock = DockStyle.Fill;  
+            this.Controls.Add(panelPeliculas); 
         }
 
         private void btnCambiarUsuario_Click(object sender, EventArgs e)
@@ -27,48 +37,43 @@ namespace GestorSalas
 
 
 
-        private void btnBuscarFuncion_Click(object sender, EventArgs e)
-        {
+        //private void btnBuscarFuncion_Click(object sender, EventArgs e)
+        //{
 
 
-            FuncionesModelo funcionesModelo = new FuncionesModelo();
-            GenerarTicketServicio ticketServicio = new GenerarTicketServicio();
-            
+        //    FuncionesModelo funcionesModelo = new FuncionesModelo();
+        //    GenerarTicketServicio ticketServicio = new GenerarTicketServicio();
 
 
+        //    // Verifica si hay alguna fila seleccionada
+        //    if (peliculasDgv.CurrentRow != null)
+        //    {
+        //        // Obtiene la fila seleccionada
+        //        DataGridViewRow filaSeleccionada = peliculasDgv.CurrentRow;
+
+        //        // Suponiendo que la columna de ID se llama "ID_Pelicula"
+        //        string idPelicula = filaSeleccionada.Cells["ID_Pelicula"].Value.ToString();
+        //        Console.WriteLine($"ID de la película seleccionada: {idPelicula}");
+
+        //        funcionesModelo.pelicula = filaSeleccionada.Cells["Nombre"].Value.ToString();
 
 
-
-
-            // Verifica si hay alguna fila seleccionada
-            if (peliculasDgv.CurrentRow != null)
-            {
-                // Obtiene la fila seleccionada
-                DataGridViewRow filaSeleccionada = peliculasDgv.CurrentRow;
-
-                // Suponiendo que la columna de ID se llama "ID_Pelicula"
-                string idPelicula = filaSeleccionada.Cells["ID_Pelicula"].Value.ToString();
-                Console.WriteLine($"ID de la película seleccionada: {idPelicula}");
-
-                funcionesModelo.pelicula = filaSeleccionada.Cells["Nombre"].Value.ToString();
-
-
-                ticketServicio.funciones = funcionesModelo;
-                // Crea una instancia del siguiente formulario y pasa el ID
-                Funciones funciones = new Funciones(idPelicula, ticketServicio);
+        //        ticketServicio.funciones = funcionesModelo;
+        //        // Crea una instancia del siguiente formulario y pasa el ID
+        //        Funciones funciones = new Funciones(idPelicula, ticketServicio);
 
 
                 
-                this.Hide();
-                funciones.Owner = this;
-                funciones.ShowDialog();
-                this.Show();
-            }
-            else
-            {
-                MessageBox.Show("Por favor, selecciona una película.");
-            }
-        }
+        //        this.Hide();
+        //        funciones.Owner = this;
+        //        funciones.ShowDialog();
+        //        this.Show();
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Por favor, selecciona una película.");
+        //    }
+        //}
 
 
         private void Peliculas_Load(object sender, EventArgs e)
@@ -123,12 +128,104 @@ namespace GestorSalas
 
         }
 
+
+
+
         private void Peliculas_Activated_1(object sender, EventArgs e)
         {
-            baseDatosServicios baseDatosServicios = new baseDatosServicios();
+ 
             DataTable dt = baseDatosServicios.peliculasInformacion();
-            peliculasDgv.DataSource = dt;
-            peliculasDgv.Columns["ID_Pelicula"].Visible = false;
+
+      //limpieza de PictureBox
+            panelPeliculas.Controls.Clear();
+
+            // desplazamiento vertical
+            int yOffset = 10;
+            int xOffset = 10;
+
+            // Iterariones
+            foreach (DataRow row in dt.Rows)
+            {
+                
+
+
+
+                string nombrePelicula = row["Nombre"].ToString();
+                string idPelicula = row["ID_Pelicula"].ToString();
+
+                string imagePath = Path.Combine(Application.StartupPath, "Assets", nombrePelicula + ".jpg");
+
+             
+
+
+
+                if (File.Exists(imagePath))
+                {
+                    // Crear un nuevo PictureBox para cada película
+                    PictureBox pictureBox = new PictureBox
+                    {
+                        Width = 150,  
+                        Height = 200, 
+                        Left = xOffset,    
+                        Top = yOffset, 
+                        ImageLocation = imagePath, 
+                        SizeMode = PictureBoxSizeMode.StretchImage // Ajuste de imagen
+                    };
+
+                    pictureBox.Tag = new { ID_Pelicula = idPelicula, Nombre = nombrePelicula };
+                    pictureBox.Click += new EventHandler(PictureBox_Click);
+
+
+
+           
+                    panelPeliculas.Controls.Add(pictureBox);
+
+                   
+             
+                    xOffset += pictureBox.Width + 10;
+
+                }
+                else
+                {
+                   
+                    Console.WriteLine($"Imagen no encontrada para la película: {nombrePelicula}");
+                }
+            }
+
+
+        }
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+
+
+
+            PictureBox pictureBox = sender as PictureBox;
+
+            if (pictureBox != null)
+            {
+          
+                dynamic peliculaData = pictureBox.Tag;
+                string idPelicula = peliculaData.ID_Pelicula;
+                string nombrePelicula = peliculaData.Nombre;
+
+             
+                FuncionesModelo funcionesModelo = new FuncionesModelo();
+                GenerarTicketServicio ticketServicio = new GenerarTicketServicio();
+
+                
+                funcionesModelo.pelicula = nombrePelicula;
+
+                ticketServicio.funciones = funcionesModelo;
+
+      
+                Funciones funciones = new Funciones(idPelicula, ticketServicio);
+
+              
+                this.Hide();
+                funciones.Owner = this;
+                funciones.ShowDialog();
+                this.Show();
+            }
         }
     }
 }
